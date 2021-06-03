c3be0856e75d6c858b080796d75581c310057842
liranai <GwenvVlokhoven@gmail.com>
2021-06-03 20:13:33 +0200

    Installs forge-server, sets permissions and adds nobody user to use forged with sudo NOPASSWD

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,25 @@
-FROM archlinux
+FROM gwynethstark/arch-base-yay:latest
 LABEL maintainer="Gwyneth Stark"
 
-ENV yay_version=10.2.2-4
+# Defining requirements
+ENV pacman_packages="jre8-openjdk-headless jre11-openjdk-headless screen rsync"
 
+# Defining AUR package
+ENV aur_packages="forge-server"
+
+# Adding install script
 ADD build/*.sh /root/
 
+# Adding init script
+ADD binhex/*.sh /usr/local/bin/
+
+# Installation
 RUN chmod +x /root/*.sh && /bin/bash /root/install.sh
+
+# Mapping /config to host config for persistence
+VOLUME /config
+
+# Exposing the port for Minecraft
+EXPOSE 25565
+
+CMD ["/usr/bin/bash"]
--- build/install.sh
+++ build/install.sh
@@ -4,132 +4,195 @@
 
 set -e
 
-# refresh pacman keys since keyservers are unreliable
-#count=0
-#until pacman-key --refresh || (( count++ >= 6 ))
-#do
-#    echo "[warn] failed to refresh keys for pacman, retrying in 30 seconds..."
-#    sleep 30s
-#done
+# install required pacman packages
+pacman -S ${pacman_packages} --noconfirm --needed
 
-# force refresh of pacman db and install sed
-pacman -Sy sed --noconfirm
+# install required aur packages
+su nobody -c "yay -S ${aur_packages} --noconfirm --needed"
 
-# configure pacman to not extract certain folders from packages being installed
-# this is done as we strip out locale, man, docs etc when we build the arch-scratch image
-sed -i '\~\[options\]~a # Do not extract the following folders from any packages being installed\n'\
-'NoExtract   = usr/share/locale* !usr/share/locale/en* !usr/share/locale/locale.alias\n'\
-'NoExtract   = usr/share/doc*\n'\
-'NoExtract   = usr/share/man*\n'\
-'NoExtract   = usr/share/gtk-doc*\n' \
-'/etc/pacman.conf'
+# container perms
+####
 
-# list all packages that we want to exclude/remove
-unneeded_packages="\
-filesystem \
-cryptsetup \
-device-mapper \
-dhcpcd \
-iproute2 \
-jfsutils \
-libsystemd \
-linux \
-lvm2 \
-man-db \
-man-pages \
-mdadm \
-netctl \
-openresolv \
-pciutils \
-pcmciautils \
-reiserfsprogs \
-s-nail \
-systemd \
-systemd-sysvcompat \
-usbutils \
-xfsprogs"
+# define comma separated list of paths 
+install_paths="/srv/forge,/home/nobody"
 
-# split space separated string into list for install paths
-IFS=' ' read -ra unneeded_packages_list <<< "${unneeded_packages}"
+# split comma separated string into list for install paths
+IFS=',' read -ra install_paths_list <<< "${install_paths}"
 
-# construct string to ensure removal of any packages that might be part of tarball
-pacman_remove_unneeded_packages='pacman --noconfirm -Rsc'
+# process install paths in the list
+for i in "${install_paths_list[@]}"; do
 
-for i in "${unneeded_packages_list[@]}"; do
-	pacman_remove_unneeded_packages="${pacman_remove_unneeded_packages} ${i}"
+	# confirm path(s) exist, if not then exit
+	if [[ ! -d "${i}" ]]; then
+		echo "[crit] Path '${i}' does not exist, exiting build process..." ; exit 1
+	fi
+
 done
 
-echo "[info] Removing unneeded packages that might be part of the tarball..."
-echo "${pacman_remove_unneeded_packages} || true"
-eval "${pacman_remove_unneeded_packages} || true"
+# convert comma separated string of install paths to space separated, required for chmod/chown processing
+install_paths=$(echo "${install_paths}" | tr ',' ' ')
 
-echo "[info] Adding required packages to pacman ignore package list to prevent upgrades..."
+# set permissions for container during build - Do NOT double quote variable for install_paths otherwise this will wrap space separated paths as a single string
+chmod -R 775 ${install_paths}
 
-# add coreutils to pacman ignore list to prevent permission denied issue on Docker Hub - 
-# https://gitlab.archlinux.org/archlinux/archlinux-docker/-/issues/32
-#
-# add filesystem to pacman ignore list to prevent buildx issues with
-# /etc/hosts and /etc/resolv.conf being read only, see issue -
-# https://github.com/moby/buildkit/issues/1267#issuecomment-768903038
-#
-sed -i -e 's~#IgnorePkg.*~IgnorePkg = coreutils filesystem~g' '/etc/pacman.conf'
+# create file with contents of here doc, note EOF is NOT quoted to allow us to expand current variable 'install_paths'
+# we use escaping to prevent variable expansion for PUID and PGID, as we want these expanded at runtime of init.sh
+cat <<EOF > /tmp/permissions_heredoc
 
-pacman -Syu --noconfirm
+# get previous puid/pgid (if first run then will be empty string)
+previous_puid=\$(cat "/root/puid" 2>/dev/null || true)
+previous_pgid=\$(cat "/root/pgid" 2>/dev/null || true)
 
-echo "[info] installing primary packages..."
+# if first run (no puid or pgid files in /tmp) or the PUID or PGID env vars are different 
+# from the previous run then re-apply chown with current PUID and PGID values.
+if [[ ! -f "/root/puid" || ! -f "/root/pgid" || "\${previous_puid}" != "\${PUID}" || "\${previous_pgid}" != "\${PGID}" ]]; then
 
-# installing go since it is a requirement for Yay
-# https://aur.archlinux.org/packages/yay/
-pacman -S git supervisor sudo base-devel git go --noconfirm
+	# set permissions inside container - Do NOT double quote variable for install_paths otherwise this will wrap space separated paths as a single string
+	chown -R "\${PUID}":"\${PGID}" ${install_paths}
 
-# adding 'nobody' to primary group 'users'
-usermod -g users nobody
+fi
 
-# adding 'nobody' to group 'nobody'
-usermod -a -G nobody nobody
+# write out current PUID and PGID to files in /root (used to compare on next run)
+echo "\${PUID}" > /root/puid
+echo "\${PGID}" > /root/pgid
 
-# set up environment for user 'nobody'
-mkdir -p '/home/nobody'
-chown -R nobody:users '/home/nobody'
-chmod -R 775 '/home/nobody'
+EOF
 
-# setting home directory for user 'nobody'
-usermod -d /home/nobody nobody
+# replace permissions placeholder string with contents of file (here doc)
+sed -i '/# PERMISSIONS_PLACEHOLDER/{
+    s/# PERMISSIONS_PLACEHOLDER//g
+    r /tmp/permissions_heredoc
+}' /usr/local/bin/init.sh
+rm /tmp/permissions_heredoc
 
-# setting shell for user 'nobody'
-chsh -s /bin/bash nobody
+# env vars
+####
 
-# allow 'nobody' to run pacman without password
-echo 'nobody ALL=NOPASSWD: /usr/sbin/pacman' > /etc/sudoers.d/yay
+cat <<'EOF' > /tmp/envvars_heredoc
+export CREATE_BACKUP_HOURS=$(echo "${CREATE_BACKUP_HOURS}" | sed -e 's~^[ \t]*~~;s~[ \t]*$~~')
+if [[ ! -z "${CREATE_BACKUP_HOURS}" ]]; then
+	echo "[info] CREATE_BACKUP_HOURS defined as '${CREATE_BACKUP_HOURS}'" | ts '%Y-%m-%d %H:%M:%.S'
+else
+	echo "[info] CREATE_BACKUP_HOURS not defined,(via -e CREATE_BACKUP_HOURS), defaulting to '12'" | ts '%Y-%m-%d %H:%M:%.S'
+	export CREATE_BACKUP_HOURS="12"
+fi
+export PURGE_BACKUP_DAYS=$(echo "${PURGE_BACKUP_DAYS}" | sed -e 's~^[ \t]*~~;s~[ \t]*$~~')
+if [[ ! -z "${PURGE_BACKUP_DAYS}" ]]; then
+	echo "[info] PURGE_BACKUP_DAYS defined as '${PURGE_BACKUP_DAYS}'" | ts '%Y-%m-%d %H:%M:%.S'
+else
+	echo "[info] PURGE_BACKUP_DAYS not defined,(via -e PURGE_BACKUP_DAYS), defaulting to '14'" | ts '%Y-%m-%d %H:%M:%.S'
+	export PURGE_BACKUP_DAYS="14"
+fi
+export ENABLE_WEBUI_CONSOLE=$(echo "${ENABLE_WEBUI_CONSOLE}" | sed -e 's~^[ \t]*~~;s~[ \t]*$~~')
+if [[ ! -z "${ENABLE_WEBUI_CONSOLE}" ]]; then
+	echo "[info] ENABLE_WEBUI_CONSOLE defined as '${ENABLE_WEBUI_CONSOLE}'" | ts '%Y-%m-%d %H:%M:%.S'
+else
+	echo "[info] ENABLE_WEBUI_CONSOLE not defined,(via -e ENABLE_WEBUI_CONSOLE), defaulting to 'yes'" | ts '%Y-%m-%d %H:%M:%.S'
+	export ENABLE_WEBUI_CONSOLE="yes"
+fi
+if [[ "${ENABLE_WEBUI_CONSOLE}" == "yes" ]]; then
+	export ENABLE_WEBUI_AUTH=$(echo "${ENABLE_WEBUI_AUTH}" | sed -e 's~^[ \t]*~~;s~[ \t]*$~~')
+	if [[ ! -z "${ENABLE_WEBUI_AUTH}" ]]; then
+		echo "[info] ENABLE_WEBUI_AUTH defined as '${ENABLE_WEBUI_AUTH}'" | ts '%Y-%m-%d %H:%M:%.S'
+	else
+		echo "[warn] ENABLE_WEBUI_AUTH not defined (via -e ENABLE_WEBUI_AUTH), defaulting to 'yes'" | ts '%Y-%m-%d %H:%M:%.S'
+		export ENABLE_WEBUI_AUTH="yes"
+	fi
+	if [[ $ENABLE_WEBUI_AUTH == "yes" ]]; then
+		export WEBUI_USER=$(echo "${WEBUI_USER}" | sed -e 's~^[ \t]*~~;s~[ \t]*$~~')
+		if [[ ! -z "${WEBUI_USER}" ]]; then
+			echo "[info] WEBUI_USER defined as '${WEBUI_USER}'" | ts '%Y-%m-%d %H:%M:%.S'
+		else
+			echo "[warn] WEBUI_USER not defined (via -e WEBUI_USER), defaulting to 'admin'" | ts '%Y-%m-%d %H:%M:%.S'
+			export WEBUI_USER="admin"
+		fi
+		export WEBUI_PASS=$(echo "${WEBUI_PASS}" | sed -e 's~^[ \t]*~~;s~[ \t]*$~~')
+		if [[ ! -z "${WEBUI_PASS}" ]]; then
+			if [[ "${WEBUI_PASS}" == "minecraft" ]]; then
+				echo "[warn] WEBUI_PASS defined as '${WEBUI_PASS}' is weak, please consider using a stronger password" | ts '%Y-%m-%d %H:%M:%.S'
+			else
+				echo "[info] WEBUI_PASS defined as '${WEBUI_PASS}'" | ts '%Y-%m-%d %H:%M:%.S'
+			fi
+		else
+			WEBUI_PASS_file="/config/minecraft/security/WEBUI_PASS"
+			if [ ! -f "${WEBUI_PASS_file}" ]; then
+				# generate random password for web ui using SHA to hash the date,
+				# run through base64, and then output the top 16 characters to a file.
+				mkdir -p "/config/minecraft/security" ; chown -R nobody:users "/config/minecraft"
+				date +%s | sha256sum | base64 | head -c 16 > "${WEBUI_PASS_file}"
+			fi
+			echo "[warn] WEBUI_PASS not defined (via -e WEBUI_PASS), using randomised password (password stored in '${WEBUI_PASS_file}')" | ts '%Y-%m-%d %H:%M:%.S'
+			export WEBUI_PASS="$(cat ${WEBUI_PASS_file})"
+		fi
+	fi
+	export WEBUI_CONSOLE_TITLE=$(echo "${WEBUI_CONSOLE_TITLE}" | sed -e 's~^[ \t]*~~;s~[ \t]*$~~')
+	if [[ ! -z "${WEBUI_CONSOLE_TITLE}" ]]; then
+		echo "[info] WEBUI_CONSOLE_TITLE defined as '${WEBUI_CONSOLE_TITLE}'" | ts '%Y-%m-%d %H:%M:%.S'
+	else
+		echo "[info] WEBUI_CONSOLE_TITLE not defined,(via -e WEBUI_CONSOLE_TITLE), defaulting to 'Minecraft Java'" | ts '%Y-%m-%d %H:%M:%.S'
+		export WEBUI_CONSOLE_TITLE="Minecraft Java"
+	fi
+fi
+export CUSTOM_JAR_PATH=$(echo "${CUSTOM_JAR_PATH}" | sed -e 's~^[ \t]*~~;s~[ \t]*$~~')
+if [[ ! -z "${CUSTOM_JAR_PATH}" ]]; then
+	echo "[info] CUSTOM_JAR_PATH defined as '${CUSTOM_JAR_PATH}'" | ts '%Y-%m-%d %H:%M:%.S'
+else
+	echo "[info] CUSTOM_JAR_PATH not defined,(via -e CUSTOM_JAR_PATH), defaulting to '/config/minecraft/minecraft_server.jar' (Mojang Minecraft Java)" | ts '%Y-%m-%d %H:%M:%.S'
+	export CUSTOM_JAR_PATH="/config/minecraft/minecraft_server.jar"
+fi
+export JAVA_VERSION=$(echo "${JAVA_VERSION}" | sed -e 's~^[ \t]*~~;s~[ \t]*$~~')
+if [[ ! -z "${JAVA_VERSION}" ]]; then
+	echo "[info] JAVA_VERSION defined as '${JAVA_VERSION}'" | ts '%Y-%m-%d %H:%M:%.S'
+else
+	echo "[info] JAVA_VERSION not defined,(via -e JAVA_VERSION), defaulting to '8'" | ts '%Y-%m-%d %H:%M:%.S'
+	export JAVA_VERSION="8"
+fi
+if [[ "${JAVA_VERSION}" == "8" ]]; then
+	ln -fs /usr/lib/jvm/java-8-openjdk/jre/bin/java /usr/bin/java
+	archlinux-java set java-8-openjdk/jre
+elif [[ "${JAVA_VERSION}" == "11" ]]; then
+	ln -fs /usr/lib/jvm/java-11-openjdk/bin/java /usr/bin/java
+	archlinux-java set java-11-openjdk
+else
+	echo "[warn] Java version '${JAVA_VERSION}' not installed, defaulting to Java version 8" | ts '%Y-%m-%d %H:%M:%.S'
+	ln -fs /usr/lib/jvm/java-8-openjdk/jre/bin/java /usr/bin/java
+	archlinux-java set java-8-openjdk/jre
+fi
+export JAVA_HOME=$(readlink -f /usr/bin/java | sed "s:bin/java::")
+export JAVA_INITIAL_HEAP_SIZE=$(echo "${JAVA_INITIAL_HEAP_SIZE}" | sed -e 's~^[ \t]*~~;s~[ \t]*$~~')
+if [[ ! -z "${JAVA_INITIAL_HEAP_SIZE}" ]]; then
+	echo "[info] JAVA_INITIAL_HEAP_SIZE defined as '${JAVA_INITIAL_HEAP_SIZE}'" | ts '%Y-%m-%d %H:%M:%.S'
+else
+	echo "[info] JAVA_INITIAL_HEAP_SIZE not defined,(via -e JAVA_INITIAL_HEAP_SIZE), defaulting to '512M'" | ts '%Y-%m-%d %H:%M:%.S'
+	export JAVA_INITIAL_HEAP_SIZE="512M"
+fi
+export JAVA_MAX_HEAP_SIZE=$(echo "${JAVA_MAX_HEAP_SIZE}" | sed -e 's~^[ \t]*~~;s~[ \t]*$~~')
+if [[ ! -z "${JAVA_MAX_HEAP_SIZE}" ]]; then
+	echo "[info] JAVA_MAX_HEAP_SIZE defined as '${JAVA_MAX_HEAP_SIZE}'" | ts '%Y-%m-%d %H:%M:%.S'
+else
+	echo "[info] JAVA_MAX_HEAP_SIZE not defined,(via -e JAVA_MAX_HEAP_SIZE), defaulting to '1024M'" | ts '%Y-%m-%d %H:%M:%.S'
+	export JAVA_MAX_HEAP_SIZE="1024M"
+fi
+export JAVA_MAX_THREADS=$(echo "${JAVA_MAX_THREADS}" | sed -e 's~^[ \t]*~~;s~[ \t]*$~~')
+if [[ ! -z "${JAVA_MAX_THREADS}" ]]; then
+	echo "[info] JAVA_MAX_THREADS defined as '${JAVA_MAX_THREADS}'" | ts '%Y-%m-%d %H:%M:%.S'
+else
+	echo "[info] JAVA_MAX_THREADS not defined,(via -e JAVA_MAX_THREADS), defaulting to '1'" | ts '%Y-%m-%d %H:%M:%.S'
+	export JAVA_MAX_THREADS="1"
+fi
+export STARTUP_CMD=$(echo "${STARTUP_CMD}" | sed -e 's~^[ \t]*~~;s~[ \t]*$~~')
+if [[ ! -z "${STARTUP_CMD}" ]]; then
+	echo "[info] STARTUP_CMD defined as '${STARTUP_CMD}'" | ts '%Y-%m-%d %H:%M:%.S'
+else
+	echo "[info] STARTUP_CMD not defined (via -e STARTUP_CMD)" | ts '%Y-%m-%d %H:%M:%.S'
+fi
+EOF
 
-# clone yay as 'nobody' to home folder and build
-su nobody -c "git clone https://aur.archlinux.org/yay.git /home/nobody/yay && cd /home/nobody/yay && makepkg"
+# replace env vars placeholder string with contents of file (here doc)
+sed -i '/# ENVVARS_PLACEHOLDER/{
+    s/# ENVVARS_PLACEHOLDER//g
+    r /tmp/envvars_heredoc
+}' /usr/local/bin/init.sh
+rm /tmp/envvars_heredoc
 
-# install yay as root
-pacman -U /home/nobody/yay/yay-${yay_version}-x86_64.pkg.tar.zst --noconfirm
-
-# remove base devel excluding useful core packages
-pacman -Ru $(pacman -Qgq base-devel | grep -v awk | grep -v pacman | grep -v sed | grep -v grep | grep -v gzip | grep -v which | grep -v fakeroot) --noconfirm
-
-# general cleanup to shrink image size
-yes|pacman -Scc
-pacman --noconfirm -Rns $(pacman -Qtdq) 2> /dev/null || true
-rm -rf /var/cache/* \
-/var/empty/.cache/* \
-/usr/share/locale/* \
-/usr/share/man/* \
-/usr/share/gtk-doc/* \
-/tmp/* \
-/home/nobody/* \
-/home/nobody/.cache/*
-
-# additional cleanup for base only
-rm -rf /root/* \
-/var/cache/pacman/pkg/* \
-/usr/lib/firmware \
-/usr/lib/modules \
-/.dockerenv \
-/.dockerinit \
-/usr/share/info/* \
-/README \
-/bootstrap
+echo 'nobody ALL=NOPASSWD: /usr/bin/forged' > /etc/sudoers.d/forged
